7ef0a4c8cfc5e1625b2f7835a0d82e8c1e8b1bbc
RAPHAEL <rhbmraphael@gmail.com>
2025-05-03 15:46:05 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Configuração do Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBkj8v3e2ffn2wqVSHh6oKnToUbXoe5W-c64Rr8",
+  authDomain: "agenda-eventos-ccb.firebaseapp.com",
+  databaseURL: "https://agenda-eventos-ccb-default-rtdb.firebaseio.com",
+  projectId: "agenda-eventos-ccb",
+  storageBucket: "agenda-eventos-ccb.appspot.com",
+  messagingSenderId: "906457271828",
+  appId: "1:906457271828:web:36f370bb64d94d219f6f30"
+};
