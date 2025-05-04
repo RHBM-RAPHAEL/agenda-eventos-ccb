8bd8bca22cc697683ca9c06a48842f5bed4a280b
RAPHAEL <rhbmraphael@gmail.com>
2025-05-04 14:55:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -26,8 +26,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
-const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 const auth = getAuth();
 
