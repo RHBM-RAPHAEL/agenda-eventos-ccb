b817d19d729d88dacbbd8e57fa529398fa7453b0
RAPHAEL <rhbmraphael@gmail.com>
2025-05-04 13:54:54 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,22 @@
-export const firebaseConfig = {
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
   apiKey: "AIzaSyCe3EqDWGXF9cR8mzCrOb_yryaWzsCuRaM",
   authDomain: "agenda-eventos-ccb.firebaseapp.com",
   databaseURL: "https://agenda-eventos-ccb-default-rtdb.firebaseio.com",
   projectId: "agenda-eventos-ccb",
-  storageBucket: "agenda-eventos-ccb.appspot.com",
+  storageBucket: "agenda-eventos-ccb.firebasestorage.app",
   messagingSenderId: "325923477189",
   appId: "1:325923477189:web:1aba52c8119d290338c2ac",
   measurementId: "G-PDTYLHH85J"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
